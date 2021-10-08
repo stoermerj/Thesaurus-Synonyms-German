681b0a89f42ba09506c7044d70ad2c9fde67e6a1
stoermerj <61618816+stoermerj@users.noreply.github.com>
2021-10-08 15:43:19 +0200

    Create ThesaurusTesting.js

---
+++ ThesaurusTesting.js
@@ -0,0 +1,31 @@
+//getting the thesaurus terms for a synonym
+
+//define the term
+let term = 'polieren'
+
+//define the URL to call
+function defineURL(search) {
+    let newUrl = 'https://www.openthesaurus.de/synonyme/search?q=' + search + '&format=application/json'
+    return (newUrl)
+}
+
+//call the function
+let url = defineURL(term)
+
+//call the api
+fetch(url)
+    .then(x => x.json())
+    .then(function (y) {
+        if (y.synsets.length > 0) {
+            for (let i = 0; i < y.synsets.length; i++) {
+                for (let m = 0; m < y.synsets[i].terms.length; m++) {
+                    console.log(y.synsets[i].terms[m].term)
+                }
+            }
+        }
+        else {
+            console.log('no synonyms found')
+        }
+    })
+
+
